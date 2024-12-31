7c00e3f0f0338d862dd452a451d58e1bc7cc78f0
David Feinzimer <dfeinzimer@esri.com>
2024-12-30 17:09:12 -0800

    Update CodeScanner.swift

--- Sources/ArcGISToolkit/Common/CodeScanner.swift
+++ Sources/ArcGISToolkit/Common/CodeScanner.swift
@@ -16,6 +16,7 @@
 import SwiftUI
 
 /// Scans machine readable information like QR codes and barcodes.
+//@available(visionOS, unavailable)
 struct CodeScanner: View {
     @Binding var code: String
     
